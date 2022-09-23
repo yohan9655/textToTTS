e798827790ddd2cfa957c1b9a01e7ac4ec84c58f
Yohan Varghese Kuriakose <yohan@Yohans-MBP.lan>
2022-09-23 03:37:01 -0700

    Fixed port error

--- Take_home_assignment/index.js
+++ Take_home_assignment/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3030;
+const port = process.env.PORT || 3030;
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
